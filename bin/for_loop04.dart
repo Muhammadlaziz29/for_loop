d2d7add192110bde7bdeb9d5c40ab9e2692ec002
Muhammadlaziz29 <muxammadlaziz29@gmail.com>
2022-08-22 12:41:04 +0500

    hello

--- bin/for_loop04.dart
+++ bin/for_loop04.dart
@@ -1,4 +1,4 @@
-int func(List<int> numbers) {
+
 /*
    Given the list of numbers, return the sum the even numbers in the list
 Args:
@@ -6,7 +6,20 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+
+
+int func(List<int> numbers) {
+    int x = 0;
+    for(int i = 0; i<numbers.length; i+=1 ) {
+      if (numbers[i]%2 == 0 ) {
+        x+=numbers[i];
+      }
+    }
+
+  return x;
 }
 
-void main() {}
+void main() {
+  print(func([5,3,2,8,5]));
+}
+
